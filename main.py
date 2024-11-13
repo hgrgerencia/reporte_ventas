c1a038fe9d0bfaf5a1b78380b7f197188c03097f
hgrgerencia <145581011+hgrgerencia@users.noreply.github.com>
2024-11-13 15:44:53 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -14,18 +14,23 @@ st.set_page_config(
 def menu():
     # Crear el menú lateral
     USER=st.secrets["USER"]
-    st.sidebar.write(f"Bienvenido {USER}")
+    st.sidebar.write(f"Bienvenido... ")
     st.sidebar.image("img/ribs.jpg",width=140, use_container_width=True)
     st.sidebar.divider()
+    USUARIO = st.sidebar.text_input("Nombre usuario: ")
     # Lista de páginas
-    indice = {
-        "Inicio": paginareporte,
-        "Mesoneros": paginamesoneros,
-        "Contacto": paginacontacto,
-    }
-    st.sidebar.title("Menú")
-    menu = st.sidebar.radio("Navega a:", list(indice.keys()))
-    indice[menu]()
+    if USUARIO == USER:
+        indice = {
+            "Inicio": paginareporte,
+            "Mesoneros": paginamesoneros,
+            "Contacto": paginacontacto,
+        }
+        st.sidebar.title("Menú")
+        menu = st.sidebar.radio("Navega a:", list(indice.keys()))
+        indice[menu]()
+    else:
+        st.write("Favor ingresar su nombre de usuario en el Sidebar")
+    
 
 menu()
 
