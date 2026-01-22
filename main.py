8254793a226452cccf93ac2c6882bcb35a0d29e8
hgrgerencia <145581011+hgrgerencia@users.noreply.github.com>
2026-01-22 07:33:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def menu():
     # Crear el menú lateral
     USER=st.secrets["USER"]
     st.sidebar.write(f"Bienvenido... ")
-    st.sidebar.image("img/ribs.jpg",width=140, use_container_width=True)
+    st.sidebar.image("img/ribs.jpg",width=140)
     st.sidebar.divider()
     USUARIO = st.sidebar.text_input("Nombre usuario: ")
     # Lista de páginas
@@ -40,6 +40,7 @@ def menu():
 
     
 menu()
+
 
 
 
